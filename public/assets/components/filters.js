baf76d501a3e1146fc216c99e4a06b945da9f58d
Ali0908 <zaabat97@gmail.com>
2022-11-27 12:08:24 +0100

    finalization of the filter

--- public/assets/components/filters.js
+++ public/assets/components/filters.js
@@ -1,45 +1,78 @@
-const carTable = document.getElementById('cars-table');
-const suvButton = document.querySelector("#SUV")
-let hpCarsbySuvCategory = [];
+const suvList = document.getElementById('cars-table');
+const suvBtn = document.getElementById('SUV');
+const fullElectricBtn = document.getElementById('Full_Electric');
+const sportbackBtn = document.getElementById('Sportback');
+const allBtn = document.getElementById('All');
+let hpSuvCar = [];
 
-function suvfunction(){
-  const loadCarsbyCategories = async () => {
-    try {
-        const res = await fetch('http://localhost:8000/list');
-        hpCars = await res.json();
-        displayCarsbyCategories(hpCarsbySuvCategory);
-    } catch (err) {
-        console.error(err);
-    }
-  };
-}
-suvButton.addEventListener('click', suvfunction,false);
-  // const suvButtonString = e.target.value.toLowerCase();
-  // const filteredCarsbySuvCategory = hpCarsbySuvCategory.filter((car) => {
-  //     return (
-  //         car.name.toLowerCase().includes(suvButtonString)
-  //     ); 
-  //   });
-  //     displayCarsbyCategories(filteredCarsbySuvCategory);  
-});
-
-
-
-const displayCarsbyCategories = (cars) => {
-  const htmlString = cars
-      .map((car) => {
-          return `
-          <tr>
-              <th>${car.name}</th>
-              <th>${car.nbreSeats}</th>
-              <th>${car.nbreDoors}</th>
-              <th>${car.cost}</th>
-              <th>${car.category.name}</th>
-          <tr>
-      `;
-      })
-      .join('');
-  carTable.innerHTML = htmlString;
+//Data recovery
+const loadCarsbySuvCategory = async () => {
+  try {
+      const res = await fetch('/list');
+      hpSuvCar = await res.json();
+      displayCarsbySuvCategory(hpSuvCar);
+  } catch (err) {
+      console.error(err);
+  }
 };
 
-suvfunction();
+// Setting up a listening event
+suvBtn.addEventListener('click', suvFilter)
+fullElectricBtn.addEventListener('click', fullElectricFilter)
+sportbackBtn.addEventListener('click', sportbackFilter)
+allBtn.addEventListener('click', allFilter)
+
+// Setting up filters by button
+function suvFilter (){
+    const filteredCarsbyCategory = hpSuvCar.filter((car) => {
+      if (car.category.name === "SUV") { 
+        return car.name
+      }
+    });
+    displayCarsbySuvCategory(filteredCarsbyCategory);
+};
+
+function fullElectricFilter (){
+  const filteredCarsbyCategory = hpSuvCar.filter((car) => {
+    if (car.category.name === "Full Electric") { 
+      return car.name
+    }
+  });
+  displayCarsbySuvCategory(filteredCarsbyCategory);
+};
+
+function sportbackFilter (){
+  const filteredCarsbyCategory = hpSuvCar.filter((car) => {
+    if (car.category.name === "Sportback") { 
+      return car.name
+    }
+  });
+  displayCarsbySuvCategory(filteredCarsbyCategory);
+};
+
+function allFilter (){
+  const filteredCarsbyCategory = hpSuvCar.filter((car) => {
+      return car.name
+  });
+  displayCarsbySuvCategory(filteredCarsbyCategory);
+};
+
+// Dissemination of modified data in the DOM
+const displayCarsbySuvCategory = (cars) => {
+    const htmlString = cars
+        .map((car) => {
+            return `
+            <tr>
+                <th>${car.name}</th>
+                <th>${car.nbreSeats}</th>
+                <th>${car.nbreDoors}</th>
+                <th>${car.cost}€</th>
+                <th>${car.category.name}</th>
+            <tr>
+        `;
+        })
+        .join('');
+    suvList.innerHTML = htmlString;
+};
+
+loadCarsbySuvCategory();
--- src/Form/CarType.php
+++ src/Form/CarType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Car;
+use App\Entity\Category;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
