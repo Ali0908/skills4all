401aea5fcd024dfbe70bd9f43295b4e5336b1ec6
Ali0908 <zaabat97@gmail.com>
2022-11-27 17:09:31 +0100

    updating forms

--- src/Form/CarType.php
+++ src/Form/CarType.php
@@ -4,8 +4,11 @@ namespace App\Form;
 
 use App\Entity\Car;
 use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,10 +20,38 @@ class CarType extends AbstractType
             ->add('name', TextType::class,[
                 'label' => "Car brand",
             ])
-            ->add('nbreSeats')
-            ->add('nbreDoors')
-            ->add('cost')
-        ;
+            ->add('nbreSeats', NumberType::class,[
+                'label' => "Number of seats",
+                'attr' => array(
+                    'min' => 5,
+                    'max' => 7,
+                ),
+                'html5' => true,
+                ])
+            ->add('nbreDoors', NumberType::class,[
+                'label' => "Number of doors",
+                'attr' => array(
+                    'min' => 3,
+                    'max' => 5,
+                ),
+                'html5' => true,
+                ])
+            ->add('cost', MoneyType::class,[
+                'label' => "Price",
+                'scale'=> 2,
+                'attr' => array(
+                    'min' => 1500,
+                    'max' => 3000,
+                ),
+                'html5' => true
+                ])
+            ->add('category' , EntityType::class, [
+                'label' => 'Category',
+                'choice_label' => 'name', // valeur de la prop à afficher dans les balises options
+                'class' => Category::class,
+                'multiple' => false,
+                'expanded' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
