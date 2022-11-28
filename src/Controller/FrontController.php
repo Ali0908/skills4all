010bd009899a05cbb259b4f4c1042fdbf33109aa
Ali0908 <zaabat97@gmail.com>
2022-11-28 16:27:42 +0100

    updating FrontController

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,7 +9,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+/**
+ * @Route("/index.php")
+ */
 class FrontController extends AbstractController
 {
     /**
@@ -23,7 +25,7 @@ class FrontController extends AbstractController
     }
 
     /**
-     * @Route("index.php/list", name="list", methods={"GET"})
+     * @Route("/list", name="list", methods={"GET"})
      * 
      */
     public function listCarsByName(CarRepository $carRepository): JsonResponse
