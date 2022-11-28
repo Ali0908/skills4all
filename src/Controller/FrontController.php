ec77ab4f28289eea845cf6c9401e7badc01f9fc7
Ali0908 <zaabat97@gmail.com>
2022-11-28 11:58:39 +0100

    first deployment

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -21,28 +21,6 @@ class FrontController extends AbstractController
         ["carList"=> $carRepository->findAll(),]
     );
     }
-    /**
-     * @Route("/", name="app_front")
-     */
-    public function categoryList(CategoryRepository $categoryRepository): Response
-    {
-    return $this->render(
-        'front/index.html.twig',
-        ["categoryList"=> $categoryRepository->findAll(),]
-    );
-    }
-    //  /**
-    //  * @Route("/category", name="app_category")
-    //  */
-    // public function categoryFilter(CarRepository $carRepository): Response
-    // {
-    //     $carsFilters=$carRepository->findAll();
-    //     $carFiltersArray= array($carsFilters);
-    //     $resultCarsFilters=array_unique($carFiltersArray);
-    //     return $this->json($resultCarsFilters,Response::HTTP_OK,[],["groups" => ["api_car_list"]]);
-
-    
-    // }
 
     /**
      * @Route("/list", name="list", methods={"GET"})
@@ -56,18 +34,4 @@ class FrontController extends AbstractController
         return $this->json($carsByName,Response::HTTP_OK,[],["groups" => ["api_car_list"]]);
     
     }
-    /**
-     * @Route("/display", name="display")
-     * 
-     * @return Response
-     */
-    public function displayCarsByCategoryName(CarRepository $carRepository): Response
-    {
-        $carsByCategoryName = $carRepository->findCarsByCategoryName();
-    
-        // On transmet à notre vue la liste des films
-        return $this->json($carsByCategoryName,Response::HTTP_OK,[],["groups" => ["api_car_list"]]);
-    
-    }
-
 }
