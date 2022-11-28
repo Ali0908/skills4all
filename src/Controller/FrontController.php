724e482d785a6672a55c0c018a1da7383f81d340
Ali0908 <zaabat97@gmail.com>
2022-11-28 17:24:22 +0100

    update files

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,9 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/index.php")
- */
+
 class FrontController extends AbstractController
 {
     /**
